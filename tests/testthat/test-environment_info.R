eb686f87f426f64c2a5899b4893bdf048eaa7b69
Lia Domke <lkdomke@alaska.edu>
2019-11-06 15:51:31 -0900

    adding testing for the created package
    created 3 different tests to check that the output message matched the input message.

---
+++ tests/testthat/test-environment_info.R
@@ -0,0 +1,13 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("A message is present", {
+  capture.output(result <- environment_info("A unique message"))
+  expect_match(result, "A unique message")
+})
+
+test_that("A message is present", {
+  capture.output(result <- environment_info("A unique message"))
+  expect_match(result, "A unique message")
+})
